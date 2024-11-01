e98a93167afbda957e80244b180b6de5281d521c
Hassan Kaoud <hassankaoud84@gmail.com>
2024-11-01 11:18:07 +0200

    first commit

---
+++ main.c
@@ -0,0 +1,17 @@
+#include "STD_TYPES.h"
+#include "BIT_MATH.h"
+#include "DIO_interface.h"
+
+#include "UART_interface.h"
+#include "CLCD_interface.h"
+
+#include <util/delay.h>
+
+
+
+
+int main(void)
+{
+
+	while(1);
+}
